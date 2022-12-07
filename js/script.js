42ebcc5c5fab3223de1ce833e119183e988d6a6c
Nathan Fabio <nathan_3_m@hotmail.com>
2022-12-07 14:18:41 -0300

    everything is working

--- js/script.js
+++ js/script.js
@@ -12,14 +12,28 @@ document.addEventListener('keypress', function(e){
  });
 
  const loop = setInterval(() => {
+
+    console.log(loop);    
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '47px'
+
+        clearInterval(loop);
+
     }
 
  }, 10);
